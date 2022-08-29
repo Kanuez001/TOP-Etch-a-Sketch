2ce8ab958b89b19c94c9edb0c2645c198ef0471f
Kanu Ezindu <kanuezindu@gmail.com>
2022-08-29 12:19:19 +0100

    Add prompt
    Enables users to be able to choose the number of squares per side that they want to work with

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+const divNum = prompt("Enter the number of squares per side you want to use. The maximum is 100", "16");
+const totalDivNum = divNum * divNum;
 const container = document.querySelector(".container");
+const divLength = 600/divNum;//get the appropraite div width and height depending on the number of divs per 
+//side
+
+if (divNum < 101){//Let the maximum number of squares per side be 100
+    createGrid();
+};
 
 function createGrid() {//create a certain number of divs/grid
-    for (let i = 0; i < 256; i++){
-    let gridDiv = document.createElement("div");
+    for (let i = 0; i < totalDivNum; i++){
+    const gridDiv = document.createElement("div");
     gridDiv.classList.add("grid");
-    //gridDiv.textContent = "empty";
+    gridDiv.setAttribute("style", `height: ${divLength}px; width: ${divLength}px;`);
 
     function changeDivColor(){//change the color of a div when the cursor enters/hovers
         gridDiv.style.backgroundColor = "black";
@@ -13,6 +21,3 @@ function createGrid() {//create a certain number of divs/grid
     container.appendChild(gridDiv);
     }
 };
-
-createGrid();
-
