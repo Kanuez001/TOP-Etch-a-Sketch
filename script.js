418f6e4318c91ef053fe38c0343f014a5c7ef2ae
Kanu Ezindu <kanuezindu@gmail.com>
2022-08-29 10:37:15 +0100

    Add the changeDivColor() function
    this function changes the color of a div when the mouse cursor enters it

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ function createGrid() {//create a certain number of divs/grid
     for (let i = 0; i < 256; i++){
     let gridDiv = document.createElement("div");
     gridDiv.classList.add("grid");
+    //gridDiv.textContent = "empty";
+
+    function changeDivColor(){//change the color of a div when the cursor enters/hovers
+        gridDiv.style.backgroundColor = "black";
+    };
+    gridDiv.addEventListener("mouseenter", changeDivColor);
     container.appendChild(gridDiv);
     }
 };
 
 createGrid();
-
 
